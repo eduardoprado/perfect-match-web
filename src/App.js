796feebb7cef6823db9dc81df0414bf84c7e9c43
ëduardoprado <eduardocastilhoprado@gmail.com>
2022-09-23 18:07:14 -0300

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-// import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { width } from '@mui/system';
 import React from 'react';
 // import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
 // import Routes from './routes';
 
-// const THEME = createTheme({
-//   typography: {
-//     fontFamily: 'Montserrat',
-//     fontSize: 14,
-//     fontWeightBold: 700,
-//     fontWeightLight: 300,
-//     fontWeightRegular: 400,
-//     fontWeightMedium: 500,
-//   },
-// });
+const THEME = createTheme({
+  typography: {
+    fontFamily: 'Arvo',
+    fontSize: 14,
+    fontWeightBold: 700,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>HELOO!</h1>
-    </div>
+    <GlobalStyle>
+      <div style={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              flexDirection: 'column',
+              background: 'teal',
+              width: '100vh',
+              height: '100vh',
+            }}>
+        <h1>Helllo!</h1>
+      </div>
+    </GlobalStyle>
   );
 }
 
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
+import { COLORS } from './colors';
 
-export default createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -10,12 +11,9 @@ export default createGlobalStyle`
   body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-  body, input, button {
-    font-family: 'Montserrat', sans-serif;
-    color: #f1f1f1
-  }
-  button {
-    cursor: pointer;
+    background: ${COLORS.PRIMARY};
+    font-family: 
   }
 `;
+
+export default GlobalStyle;
